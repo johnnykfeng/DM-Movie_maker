cf87ef7144265b75c94056f1e34981ca468e945a
John Feng <johnfengphd@gmail.com>
2025-03-30 17:36:56 -0700

    Update BIN_LABELS in config.py to replace bin labels with energy levels for improved clarity in data visualization

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
-# BIN_LABELS = [
-#     "20 kev",
-#     "30 kev",
-#     "50 kev",
-#     "70 kev",
-#     "90 kev",
-#     "120 kev",
-#     "Sum CC1-CC5",
-# ]
-
 BIN_LABELS = [
-    "Bin 0",
-    "Bin 1",
-    "Bin 2",
-    "Bin 3",
-    "Bin 4",
-    "Bin 5",
-    "Bin 6",
+    "20 kev",
+    "30 kev",
+    "50 kev",
+    "70 kev",
+    "90 kev",
+    "120 kev",
+    "Sum CC1-CC5",
 ]
+
+# BIN_LABELS = [
+#     "Bin 0",
+#     "Bin 1",
+#     "Bin 2",
+#     "Bin 3",
+#     "Bin 4",
+#     "Bin 5",
+#     "Bin 6",
+# ]
 
 MM_ORDER = [
     "M58618",
